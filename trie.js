e4abb06075eb773e78f31e3d8ead5b2125a8230b
Jose <jootse.84@gmail.com>
2016-04-07 17:09:51 +0800

    MQTT Trie class

---
+++ trie.js
@@ -0,0 +1,63 @@
+export default class Trie {
+
+    constructor (trie) {
+        this.trie = {
+          'aa': {
+            'aa': {
+              'cc': {},
+              'bb': {
+                'aa': { '$': true }
+              }
+            }
+          },
+          'coc': { '$': true }
+        }
+    }
+    
+    addTopic (topic) {
+        let ls = topic.split('\/').reverse()
+        this.trie = this.rec(ls, ls.pop(), this.trie)
+    }
+    
+    print () {
+        console.log(this.trie)
+    }
+    
+    rec (levels, actual, trie) {
+
+        if (levels.length === 0 && actual === '#') {
+            for (let key in trie) {
+                if (key !== '$') {
+                     trie[key] = this.rec(levels, '#', trie[key])
+                }
+            }
+            trie['$'] = true
+            return trie
+        }
+    
+        if (levels.length === 0) {
+            if (!(actual in trie)) {
+                trie[actual] = { '$': true }
+            } else {
+                trie['$'] = true
+            }
+            return trie
+        }
+
+        let node = trie
+        let level = levels.pop()
+        switch (actual) {
+        case '+':
+        		for (let key in node) {
+                if (key !== '$') {
+                     node[key] = this.rec(levels, level, trie[key])
+                }
+            }
+            break;
+        default:
+            trie = (actual in trie) ? trie[actual] : {}
+            node[actual] = this.rec(levels, level, trie)
+        }
+        return node
+    }
+}
